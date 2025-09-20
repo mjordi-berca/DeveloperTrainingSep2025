27e557739a945ff8518bfa85553e5cb14de40594
mjordi <muhamad.jordi@berca.co.id>
2025-09-20 12:11:56 +0700

    Yacht section related

---
+++ UsrYacht2/Schemas/UsrYacht2_Detail/UsrYacht2_Detail.js
@@ -0,0 +1,8 @@
+define('UsrYacht2_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrYacht2',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
